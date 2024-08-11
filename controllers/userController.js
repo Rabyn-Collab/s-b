b60a8fc0061c4040aa1f5c4287268cfb8286901a
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-08-11 07:55:47 +0545

    new upadates

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 import { User } from "../models/User.js";
 import bcrypt from 'bcrypt';
-
+import jwt from 'jsonwebtoken';
 
 export const getAllUsers = (req, res) => {
   return res.status(200).json({});
@@ -8,13 +8,26 @@ export const getAllUsers = (req, res) => {
 
 
 export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const isExist = await User.findOne({ email });
+    if (!isExist) return res.status(404).json({ message: 'user doesn\'t exist' });
 
-  // try {
-  //   await User.create(req.body);
-  //   return res.status(201).json({ message: 'successfully registere' });
-  // } catch (err) {
+    const pass = bcrypt.compareSync(password, isExist.password);
+    if (!pass) return res.status(401).json({ message: 'invalid credential' });
 
-  // }
+    const token = jwt.sign({
+      id: isExist._id,
+      isAdmin: isExist.isAdmin
+    }, 'secret');
+    return res.status(200).json({
+      token,
+      email: isExist.email,
+      fullname: isExist.fullname
+    })
+  } catch (err) {
+    return res.status(400).json({ error: `${err}` });
+  }
 
 
 }
